246ab8d1dae72225187f2dc573bac8c41719af26
cuongthieu <suzume.logs@gmail.com>
2024-11-10 22:30:21 +0700

    Update

--- src/libs/components/Layout/Sidebar/menu.ts
+++ src/libs/components/Layout/Sidebar/menu.ts
@@ -1,5 +1,10 @@
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
+import BuildIcon from '@mui/icons-material/Build'
 import CategoryIcon from '@mui/icons-material/Category'
+import DevicesIcon from '@mui/icons-material/Devices'
+import EventAvailableIcon from '@mui/icons-material/EventAvailable'
 import UserIcon from '@mui/icons-material/Person'
+import PolicyIcon from '@mui/icons-material/Policy'
 import StackedLineChartIcon from '@mui/icons-material/StackedLineChart'
 
 export type MenuType = {
@@ -30,5 +35,35 @@ export const menus: MenuType[] = [
     title: 'Gói thiết bị',
     icon: CategoryIcon,
     active_icon: CategoryIcon,
+  },
+  {
+    href: '/devices',
+    title: 'Thiết bị',
+    icon: DevicesIcon,
+    active_icon: DevicesIcon,
+  },
+  {
+    href: '/rentals',
+    title: 'Thuê',
+    icon: EventAvailableIcon,
+    active_icon: EventAvailableIcon,
+  },
+  {
+    href: '/policies',
+    title: 'Chính sách',
+    icon: PolicyIcon,
+    active_icon: PolicyIcon,
+  },
+  {
+    href: '/maintenances',
+    title: 'Bảo trì',
+    icon: BuildIcon,
+    active_icon: BuildIcon,
+  },
+  {
+    href: '/discounts',
+    title: 'Mã giảm giá',
+    icon: AttachMoneyIcon,
+    active_icon: AttachMoneyIcon,
   },
 ]
