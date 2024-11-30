60e8223476df5a2ffecad8ad6afb917c2ce90882
cuongthieu <suzume.logs@gmail.com>
2024-11-30 22:28:22 +0700

    Update

--- src/libs/components/Layout/Sidebar/menu.ts
+++ src/libs/components/Layout/Sidebar/menu.ts
@@ -3,9 +3,11 @@ import BuildIcon from '@mui/icons-material/Build'
 import CategoryIcon from '@mui/icons-material/Category'
 import DevicesIcon from '@mui/icons-material/Devices'
 import EventAvailableIcon from '@mui/icons-material/EventAvailable'
+import HistoryIcon from '@mui/icons-material/History'
 import InventoryIcon from '@mui/icons-material/Inventory'
-import UserIcon from '@mui/icons-material/Person'
+import PersonIcon from '@mui/icons-material/Person'
 import PolicyIcon from '@mui/icons-material/Policy'
+import ReportProblemIcon from '@mui/icons-material/ReportProblem'
 import StackedLineChartIcon from '@mui/icons-material/StackedLineChart'
 
 export type MenuType = {
@@ -26,34 +28,28 @@ export const menus: MenuType[] = [
     active_icon: StackedLineChartIcon,
   },
   {
-    href: '/damage-report',
-    title: 'Báo cáo hỏng',
-    icon: StackedLineChartIcon,
-    active_icon: StackedLineChartIcon,
-  },
-  {
-    href: '/users',
-    title: 'Người dùng',
-    icon: UserIcon,
-    active_icon: UserIcon,
-  },
-  {
     href: '/categories',
     title: 'Danh mục',
     icon: CategoryIcon,
     active_icon: CategoryIcon,
   },
   {
-    href: '/package',
-    title: 'Gói thiết bị',
-    icon: InventoryIcon,
-    active_icon: InventoryIcon,
+    href: '/users',
+    title: 'Người dùng',
+    icon: PersonIcon,
+    active_icon: PersonIcon,
   },
   {
     href: '/equipments',
     title: 'Thiết bị',
     icon: DevicesIcon,
     active_icon: DevicesIcon,
+  },
+  {
+    href: '/package',
+    title: 'Gói thiết bị',
+    icon: InventoryIcon,
+    active_icon: InventoryIcon,
   },
   {
     href: '/policies',
@@ -68,10 +64,22 @@ export const menus: MenuType[] = [
     active_icon: BuildIcon,
   },
   {
-    href: '/discounts',
-    title: 'Mã giảm giá',
-    icon: AttachMoneyIcon,
-    active_icon: AttachMoneyIcon,
+    href: '/damage-report',
+    title: 'Báo cáo hỏng',
+    icon: ReportProblemIcon,
+    active_icon: ReportProblemIcon,
+  },
+  {
+    href: '/repair-records',
+    title: 'Lịch sử sửa chữa',
+    icon: HistoryIcon,
+    active_icon: HistoryIcon,
+  },
+  {
+    href: '/usage-records',
+    title: 'Lịch sử sử dụng',
+    icon: HistoryIcon,
+    active_icon: HistoryIcon,
   },
   {
     href: '/rentals',
@@ -80,9 +88,9 @@ export const menus: MenuType[] = [
     active_icon: EventAvailableIcon,
   },
   {
-    href: '/repair-records',
-    title: 'Lịch sử sửa chữa',
-    icon: EventAvailableIcon,
-    active_icon: EventAvailableIcon,
+    href: '/discounts',
+    title: 'Mã giảm giá',
+    icon: AttachMoneyIcon,
+    active_icon: AttachMoneyIcon,
   },
 ]
